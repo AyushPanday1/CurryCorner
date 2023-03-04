d1c0bb66177f989468d4fc1190e5a4f8485bbc18
amanprajapat08278 <amanprajapat08278@gmail.com>
2023-03-04 23:11:00 +0530

    usercontroller

--- route/router.js
+++ route/router.js
@@ -2,6 +2,14 @@ const express = require("express")
 const router = express.Router()
 const { createShop, UpdateeShop } = require('../controller/shopController')
 const { createProduct, updateProuct } = require("../controller/productController")
+const { createUser, loginUser, getUser, updateUser } = require("../controller/userController")
+
+
+router.post("/user", createUser)
+router.post("/loginUser", loginUser)
+router.get("/user/:userId", getUser)
+router.put("/user", updateUser)
+
 
 router.post("/shop", createShop)
 
---
+++ validation/validation.js
@@ -0,0 +1,21 @@
+function validateEmail(email) {
+    let regex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
+    return ( typeof(email) !="string" || regex.test(email)) ? true :false
+}
+
+function validPassword(password) {
+    let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
+    return regex.test(password);
+}
+
+function isValidMobile(phone){
+    let regex = /^[6-9][0-9]{9}$/
+    return regex.test(phone)
+}
+
+function isValidname(firstname){
+    let result = (typeof(firstname) == "string" && /^[a-zA-Z]+$/.test(firstname))?true:false
+    return result
+}
+
+module.exports = {validateEmail, validPassword,isValidMobile, isValidname }
